5f58c6af26c69c73982a401bc90cae1fbb9afc53
Heesoo Kim <heesoo0323@gmail.com>
2021-07-16 14:49:25 -0700

    worked on some js.

--- main.js
+++ main.js
@@ -13,3 +13,28 @@ function submit(event) {
   event.preventDefault();
   $modal.setAttribute('class', 'row modal-div justify-center before');
 }
+
+// var times = [];
+// var dates = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+// var entries = {
+//   days: {
+//     dates: ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun']
+//   }
+//   // dates: ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun']
+// };
+
+// for (var d = 0; d < 24; d++) {
+//   times.push(null);
+// }
+
+// for (var i = 0; i < 7; i++) {
+//   // var datesSelected = dates[i];
+//   entries.days.dates[i] = times;
+//   console.log(times);
+// }
+
+// var data = {
+//   entries: [],
+//   editing: null,
+//   day
+// }
