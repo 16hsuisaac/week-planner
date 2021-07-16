521cac4397b14a8bc255c5a808f88eb4e566d9b3
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-07-16 15:40:25 -0700

    Completed Data Model

--- main.js
+++ main.js
@@ -12,13 +12,25 @@ function modalShow(event) {
 function submit(event) {
   event.preventDefault();
   $modal.setAttribute('class', 'row modal-div justify-center before');
+  var dataEntry = {
+    time: $time.value,
+    description: $description.value
+  };
+  data.days[$day.value].push(dataEntry);
   // console.log($day.value, $time.value, $description.value);
 
 }
 
 var data = {
-  entries: [],
-  editing: null
+  days: {
+    mon: [],
+    tues: [],
+    wed: [],
+    thurs: [],
+    fri: [],
+    sat: [],
+    sun: []
+  }
 };
 
 var previousFormEntries = localStorage.getItem('dates-entries');
@@ -32,9 +44,9 @@ window.addEventListener('beforeunload', function (event) {
   localStorage.setItem('dates-entries', entriesStringified);
 });
 
-// var $day = document.querySelector('[name="Days of the Week"]');
-// var $time = document.querySelector('[name="Time"]');
-// var $description = document.querySelector('[name="Description"]');
+var $day = document.querySelector('[name="Days of the Week"]');
+var $time = document.querySelector('[name="Time"]');
+var $description = document.querySelector('[name="Description"]');
 
 // var $form = document.querySelector('.submitButton');
 // $submitButton.addEventListener('submit', $day.value);
